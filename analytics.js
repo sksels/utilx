b96b66f4078de4e64cc704ca215ed83c39c4f47e
sksels <92101498+sksels@users.noreply.github.com>
2026-08-05 21:20:57 +0530

    Add files via upload

--- analytics.js
+++ analytics.js
@@ -1,11 +1,15 @@
 (function () {
   try {
+    var params = new URLSearchParams(location.search);
+    var source = params.get("src") || params.get("utm_source") || "";
+
     fetch("/.netlify/functions/track", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         path: location.pathname,
         referrer: document.referrer || "",
+        source: source,
       }),
     }).catch(function () {});
   } catch (e) {
